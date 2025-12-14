118dd2a76e587eadb56368e94f60e821436d7ca6
Mingi Kim <zxc12250@naver.com>
2025-12-14 23:20:47 +0900

    [level 1] Title: 푸드 파이트 대회, Time: 0.66 ms, Memory: 87.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(int[] food) {
+        StringBuilder left = new StringBuilder();
+        
+        for (int i = 1; i < food.length; i++){
+            int count = food[i] / 2;
+            
+            for (int j = 0; j < count; j++) {
+                left.append(i);
+            }
+        }
+        
+        StringBuilder answer = new StringBuilder();
+        answer.append(left);
+        answer.append("0");
+        answer.append(left.reverse());
+        
+        return answer.toString();
+    }
+}
