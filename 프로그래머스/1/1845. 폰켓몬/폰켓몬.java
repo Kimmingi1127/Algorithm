39e72016397f54960b3ba0857a9986cc1b7b5780
Mingi Kim <zxc12250@naver.com>
2026-01-15 10:00:36 +0900

    [level 1] Title: 폰켓몬, Time: 4.31 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        Set<Integer> kinds = new HashSet<>();
+        
+        for(int n : nums) {
+            kinds.add(n);
+        }
+        
+        int pick = nums.length / 2;
+        return Math.min(kinds.size(), pick);
+    }
+}
