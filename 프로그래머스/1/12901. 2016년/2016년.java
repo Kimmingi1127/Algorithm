a07f0a09a1177efff7f6568ae801ba8ccd456f00
Mingi Kim <zxc12250@naver.com>
2026-01-20 09:28:49 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int a, int b) {
+        String[] days = {"SUN","MON","TUE","WED","THU","FRI","SAT"};
+        int[] monthDays = {31,29,31,30,31,30,31,31,30,31,30,31};
+        
+        int totalDays = 0;
+        
+        for(int i = 0; i < a -1; i++){
+            totalDays += monthDays[i];
+        }
+        
+        totalDays += (b - 1);
+        
+        return days[(5 + totalDays) % 7];
+    }
+}
